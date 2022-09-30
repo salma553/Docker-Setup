084a18bbc03e0dfe885c0c61762f3f6e3d93d624
salma553 <62328380+salma553@users.noreply.github.com>
2022-09-30 11:25:16 +0530

    Update env.js

--- angular8-frontend/src/assets/env.js
+++ angular8-frontend/src/assets/env.js
@@ -2,6 +2,6 @@
     window["env"] = window["env"] || {};
   
     // Environment variables
-    window["env"]["apiUrl"] = "http://10.0.2.4:8080";
+    window["env"]["apiUrl"] = "http://20.248.220.117:8080";
     window["env"]["debug"] = true;
   })(this);
